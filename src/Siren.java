c9a97f879d27032db8df21650065050cdb000b8f
luis.gonzalez <luis.gonzalezar@sansano.usm.cl>
2023-04-17 00:51:03 -0400

    Codigo Ejemplo Stage 2, Implementación de Siren
    Paste del codigo de ejemplo Stage 2
    Implementacion de Siren
    Implementacion inicial de Central

---
+++ src/Siren.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.net.URL;
+
+public class Siren {
+    public Siren (String soundFileName){
+        try {
+            dir = new File(soundFileName).toURI().toURL();
+            isSounding = false;
+        }
+        catch (Exception exc){
+            exc.printStackTrace(System.out);
+        }
+    }
+    public void play(){
+        isSounding = true;
+        aWave= new AePlayWave(dir);
+        aWave.start();
+    }
+    public void stop(){
+        isSounding = false;
+        aWave.stopSounding();
+    }
+    public String getHeader() {
+        return "Siren";
+    }
+    public int getState() {
+        if (isSounding)
+            return 1;
+        else
+            return 0;
+    }
+    private URL dir;
+    private boolean isSounding;
+    private AePlayWave aWave;
+}
