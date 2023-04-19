3a0288bcb67e000f4c5e97de537598b4d160c92f
Bruno Vega <bruno.vega@sansano.usm.cl>
2023-04-19 19:48:19 +0000

    Person created

---
+++ src/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+    float x;
+    float y;
+    public Person(float x, float y){
+        x = x;
+        y = y;
+    }
+    public void move(String direction){
+        switch (direction){
+            case "l" -> {
+                x -= 0.5;
+            }
+            case "r" -> {
+                x += 0.5;
+            }
+            case "u" -> {
+                y -= 0.5;
+            }
+            case "d" -> {
+                y += 0.5;
+            }
+        }
+    }
+
+    public float getX(){
+        return x;
+    }
+    public float getY(){
+        return y;
+    }
+}
